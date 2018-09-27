69e75ec634eae43c2028bbc50a0f21f0ad15d80d
Dan Daneshgar <dan_daneshgar@dell.com>
2018-09-27 15:26:09 -0500

    add event list

---
+++ GirlsEmpowermentNetwork/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GirlsEmpowermentNetwork.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GirlsEmpowermentNetwork.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventService _eventService;
+
+        public EventController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _eventService.GetAll();
+            return View(list);
+        }
+    }
+}
