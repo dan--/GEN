ae2167170bcebc3f6f90b44bf9b79ef0b33d1b47
dre coldwell <dre.coldwell@dell.com>
2018-09-27 12:24:37 -0500

    updated program to use kestrel

--- GirlsEmpowermentNetwork/Program.cs
+++ GirlsEmpowermentNetwork/Program.cs
@@ -17,8 +17,11 @@ namespace GirlsEmpowermentNetwork
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
+        }
     }
 }
